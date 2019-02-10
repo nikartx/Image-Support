9e0d3107bb0e50a399d1933199344fc12bea9212
Ivan V <jvissun@gmail.com>
2019-02-10 18:52:33 +0300

    Fixed example of using library

--- app/src/main/java/ru/nikartm/image_support/MainActivity.java
+++ app/src/main/java/ru/nikartm/image_support/MainActivity.java
@@ -7,18 +7,36 @@ import android.support.v7.app.AppCompatActivity;
 import ru.nikartm.support.BadgePosition;
 import ru.nikartm.support.ImageBadgeView;
 
+/**
+ * Example of using ImageBadgeView
+ */
 public class MainActivity extends AppCompatActivity {
 
     private ImageBadgeView imageBadgeView;
+    private ImageBadgeView ibvCart;
+
+    private int value = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         imageBadgeView = findViewById(R.id.ibv_icon4);
+        ibvCart = findViewById(R.id.ibv_icon2);
+
         initIconWithBadges();
+        initCart();
     }
 
+    // Example of counter on the badge
+    private void initCart() {
+        ibvCart.setBadgeValue(value)
+                .setMaxBadgeValue(20)
+                .setLimitBadgeValue(true);
+        ibvCart.setOnClickListener(v -> ibvCart.setBadgeValue(++value));
+    }
+
+    // Initialize a badge programmatically
     private void initIconWithBadges() {
         Typeface typeface = Typeface.createFromAsset(getAssets(), "exo_regular.ttf");
         imageBadgeView.setBadgeValue(27)
@@ -32,4 +50,5 @@ public class MainActivity extends AppCompatActivity {
                 .setShowCounter(true)
                 .setBadgePadding(4);
     }
+
 }
